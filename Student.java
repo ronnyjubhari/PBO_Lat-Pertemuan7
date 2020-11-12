1fb85adf7f741c3fb7fc664698d65a872c608179
ronnyjubhari <71800550+ronnyjubhari@users.noreply.github.com>
2020-11-12 16:02:11 +0700

    Delete Student.java

--- Student.java
+++
@@ -1,9 +0,0 @@
-public class Student extends Person{
-	public Student()
-	{
-		//super( "Ini Nama", "Ini Alamat");
-		//super();
-		//super.name = "Ini Nama";
-		System.out.println("Inside Student:Constructor");
-	}
-}
