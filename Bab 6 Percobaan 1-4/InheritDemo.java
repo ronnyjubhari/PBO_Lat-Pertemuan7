55ac2b5390d89527ca2c773b8fb50b7ce5ea6d70
ronnyjubhari <71800550+ronnyjubhari@users.noreply.github.com>
2020-11-12 15:57:07 +0700

    Add files via upload

---
+++ Bab 6 Percobaan 1-4/InheritDemo.java
@@ -0,0 +1,5 @@
+public class InheritDemo{
+	public static void main(String[] args) {
+		Student Wahyu = new Student();
+	}	
+}
---
+++ Bab 6 Percobaan 1-4/Person.java
@@ -0,0 +1,31 @@
+public class Person{ 
+	protected String name; 
+	protected String address;
+	
+	public Person(){
+		System.out.println("Inside Person:Constructor");
+		name = "";
+		address = "";
+	}
+
+	public Person(String name, String address){
+		this.name = name;
+		this.address = address;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+	public String getAddress(){
+		return address;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setAddress(String add){
+		this.address = add;
+	}
+}
